d9b56fce55e3ea987ec54112db0f4d64a8564826
Davimmo <davi20bezerra@hotmail.com>
2024-11-08 14:51:31 -0300

    Alterações e adição de outro exemplo

--- Tópico 1 Q1 - Davi.py
+++ Tópico 1 Q1 - Davi.py
@@ -57,3 +57,17 @@ if resultados is not None:
     print(f"Chip de Computador: {int(c)}")
 else:
     print("O sistema não possui uma solução única ou é inviável.")
+
+#Aqui um exemplo de que resolver méta de produção serve para qualquer matriz quadrada
+# cincoXcinco=[
+#     [2,1,-1,3,2],
+#     [4,-2,5,1,3],
+#     [1,3,-3,2,1],
+#     [5,2,2,-1,4],
+#     [3,-1,4,3,-2]
+# ]
+
+# vetsolução=[10,5,-3,7,2]
+
+# resultado=resolver_meta_producao(cincoXcinco,vetsolução)
+# print(resultado)
